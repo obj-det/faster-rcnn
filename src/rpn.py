92c7a98bfff44a1c3ee35c865171d83db6389780
Rishabh Mahajani <rmahajani31@gmail.com>
2025-02-15 15:43:32 -0500

    wrote the class for the region proposal network

---
+++ src/rpn.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+
+
+class RPN(nn.Module):
+    def __init__(self, in_channels=256, num_anchors=9):
+        super(RPN, self).__init__()
+        self.initial_conv = nn.Conv2d(in_channels, 256, kernel_size=3, padding=1)
+        self.conv_cls = nn.Conv2d(256, num_anchors * 2, kernel_size=1)
+        self.conv_reg = nn.Conv2d(256, num_anchors * 4, kernel_size=1)
+
+    
+    def forward(self, x):
+        x = self.initial_conv(x)
+        cls = self.conv_cls(x)
+        reg = self.conv_reg(x)
+        return cls, reg
