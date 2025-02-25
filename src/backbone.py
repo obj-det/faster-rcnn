4886c321646584e21a0679a2ecb3227145bd876c
Rishabh Mahajani <rmahajani31@gmail.com>
2025-02-25 11:23:51 +0000

    modifying the way in which vgg16 is loaded to avoid warning

--- src/backbone.py
+++ src/backbone.py
@@ -1,11 +1,12 @@
 import torch
 import torch.nn as nn
 import torchvision.models as models
+from torchvision.models import VGG16_Weights
 
 class Backbone(nn.Module):
     def __init__(self, output_layer_map):
         super(Backbone, self).__init__()
-        self.vgg16 = models.vgg16(pretrained=True).features
+        self.vgg16 = models.vgg16(weights=VGG16_Weights.IMAGENET1K_V1).features
         # self.vgg16.eval()
         self.output_layer_map = output_layer_map
         self.features = dict()
